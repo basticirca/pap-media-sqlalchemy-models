0884d034d6ea1abd897becfc8223c799065338aa
sebastianWV <stickert@worldviz.com>
2017-10-16 12:09:28 +0200

    added database debug dir to dev config

--- config/dev.py
+++ config/dev.py
@@ -1,3 +1,5 @@
 ﻿SQLALCHEMY_ECHO = False
 
 SQLALCHEMY_DATABASE_DEBUG_URI = 'sqlite:///../pap-media-shared-files/dsa_media_control_kit.db'
+
+SQLALCHEMY_DATABASE_DEBUG_DIR = '../pap-media-shared-files'
