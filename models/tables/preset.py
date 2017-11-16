796a57f8ab98e2cd0ce70c0d81bd2965bd9e1d93
sebastianWV <stickert@worldviz.com>
2017-11-16 23:28:21 +0100

    added name to preset table

--- models/tables/preset.py
+++ models/tables/preset.py
@@ -10,9 +10,11 @@ class Preset(TableBase):
   __tablename__ = 'preset'
 
   id = Column(Integer, primary_key=True)
-
+  
+  name = Column(String(256), nullable=False)
+  
   json = Column(String(4096), nullable=False)
   
   def __repr__(self):
-    return "<Preset id=%s json=%s>" % (
-        str(self.id), self.json)
+    return "<Preset id=%s name=%s json=%s>" % (
+        str(self.id), self.name, self.json)
