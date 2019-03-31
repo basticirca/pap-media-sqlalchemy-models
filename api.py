c341599b05d8e00de5174c34844526519275e3fc
basti_circa <sebastian.stickert@gmail.com>
2019-03-31 17:09:47 +0200

    updated api

--- api.py
+++ api.py
@@ -11,102 +11,55 @@ class Api(object):
         self.bind = bind
 
     def open(self):
-        ''' creates a session for database transaction if none is active. '''
-        if not self.is_open():
-            self._db = Session(bind=self.bind)
-            
+        """ creates a session for database transaction if none is active. """
+        self._db = Session(bind=self.bind)
 
     def close(self):
-        ''' closes the current database session if open. '''
-        if self.is_open():
-            self._db.expunge_all()
-            self._db.close()
-            self._db = None
+        """ closes the current database session if open. """
+        self._db.expunge_all()
+        self._db.close()
+        self._db = None
+
+    def commit(self):
+        return self._db.commit()
 
     def _clear(self):
-        ''' clears all tables '''
-        self.ensure_open()
-
+        """ clears all tables """
         for table in reversed(database.base.TableBase.metadata.sorted_tables):
             self._db.execute(table.delete())
 
-        return self.commit()
-
-    def commit(self):
-        ''' Commits changed content left in session.
-            Automatic rollback is performed if errror occurs.
-            Returns success of transaction. '''
-        if self.is_open():
-            try:
-                self._db.commit()
-                return True
-            except exc.SQLAlchemyError as e:
-                print("FAILURE:", e)
-                self._db.rollback()
-                return False
-        else:
-            print("Notification: Session not open, nothing to commit.")
-            return False
-
-    def is_open(self):
-        ''' Returns true if the current session is active, false otherwise. '''
-        return self._db != None
-
-    def ensure_open(self):
-        ''' checks if database session is open and opens if not. '''
-        if not self.is_open():
-            self.open()
-
     def insert(self, obj):
-        ''' adds a database object to the current session. '''
-        if self.is_open():
-            self._db.add(obj)
-        else:
-            print("Notification: Session not open, nothing to add.")
-
+        """ adds a database object to the current session. """
+        self._db.add(obj)
+    
     def insert_all(self, obj_list):
-        ''' adds a list of database objects to the current session. '''
-        if self.is_open():
-            self._db.add_all(obj_list)
-        else:
-            print("Notification: Session not open, nothing to add.")
+        """ adds a list of database objects to the current session. """
+        self._db.add_all(obj_list)
 
     def expunge(self, obj):
-        ''' removes a database object from the current session. '''
-        if self.is_open():
-            self._db.expunge(obj)
-        else:
-            print("Notification: Session not open, nothing to remove.")
+        """ removes a database object from the current session. """
+        self._db.expunge(obj)
 
     def expunge_all(self, obj_list):
-        ''' removes a list of database objects from the current session. '''
-        if self.is_open():
-            for item in obj_list:
-                self._db.expunge(item)
-        else:
-            print("Notification: Session not open, nothing to remove.")
+        """ removes a list of database objects from the current session. """
+        for item in obj_list:
+            self._db.expunge(item)
 
     def delete(self, obj):
-        ''' Helper function to encapsulate delete operations.
+        """ Helper function to encapsulate delete operations.
             Performs automatic commit (see commit()).
             Performs automatic expunge if commit unsuccessful (see expunge()).
-            Returns success of operation. '''
-        self.ensure_open()
+            Returns success of operation. """
         self._db.delete(obj)
-        if not self.commit():
-            self._db.expunge(obj)
-            return False
-        return True
-    
+
     def query(self,*args):
-        ''' universal function for querying the database connection. '''
-        self.ensure_open()
+        """ universal function for querying the database connection. """
         return self._db.query(*args)
     
     def select(self, table, filter):
-        ''' helper function for performing simple select operations. '''
+        """ helper function for performing simple select operations. """
         return self.query(table).filter(filter)
 
     def select_all(self, table):
-        ''' helper function for performing select operations with no filter. '''
-        return self.query(table).filter(filter)
+        """ helper function for performing select operations with no filter. """
+        return self.query(table).all()
