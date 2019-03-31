1e116c9346d2dc84e2f18f0e0602daba3d0defe3
basti_circa <sebastian.stickert@gmail.com>
2019-03-31 16:30:20 +0200

    changed default config for database path & name

--- config/dev.py
+++ config/dev.py
@@ -1,5 +1,5 @@
 ﻿SQLALCHEMY_ECHO = False
 
-SQLALCHEMY_DATABASE_DEBUG_URI = 'sqlite:///../pap-media-shared-files/dsa_media_control_kit.db'
+SQLALCHEMY_DATABASE_DEBUG_URI = 'sqlite:///companion.db'
 
-SQLALCHEMY_DATABASE_DEBUG_DIR = '../pap-media-shared-files'
+SQLALCHEMY_DATABASE_DEBUG_DIR = './'
