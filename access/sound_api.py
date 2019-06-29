bf6ef47e7dcd61aed7926fe2ae52e5b33c95046e
sebastianWV <stickert@worldviz.com>
2019-06-30 01:25:08 +0200

    adjusted naming

--- access/sound_api.py
+++ access/sound_api.py
@@ -2,31 +2,32 @@ from database.access.api import Api
 from database import tables
 from database.tables.resource import ResourceType
 
+
 class SoundApi(Api):
     def __init__(self, bind):
         super().__init__(bind)
 
-    def add_sound_file(self, uuid, name):
+    def add_sound(self, uuid, name):
         sound = tables.Sound()
         sound.resource = tables.Resource(uuid=uuid, name=name, type=ResourceType.sound)
         self.insert(sound)
         return sound
 
-    def get_sound_file(self, uuid):
+    def get_sound(self, uuid):
         return self.select(tables.Sound, tables.Sound.uuid == uuid).first()
 
-    def get_sound_files(self):
+    def get_sounds(self):
         return self.select(tables.Sound)
 
-    def remove_sound_file(self, uuid):
-        sound = self.get_sound_file(uuid)
+    def remove_sound(self, uuid):
+        sound = self.get_sound(uuid)
         if sound is None:
             return False
         self.delete(sound.resource)
         return True
 
-    def remove_sound_files(self, uuids=[]):
+    def remove_sounds(self, uuids=[]):
         for uuid in uuids:
-            if not self.remove_sound_file(uuid):
+            if not self.remove_sound(uuid):
                 return False
         return True
