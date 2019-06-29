a0143c1e5e7ee77526b7b23d313b2776d83cdae8
sebastianWV <stickert@worldviz.com>
2019-06-29 22:43:04 +0200

    refactored Api

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 ﻿import database.base
-from database.models import tables
 from sqlalchemy import exc
 from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker()
+
 
 class Api(object):
     def __init__(self, bind):
@@ -11,102 +11,72 @@ class Api(object):
         self.bind = bind
 
     def open(self):
-        ''' creates a session for database transaction if none is active. '''
+        """ creates a session for database transaction if none is active. """
         if not self.is_open():
             self._db = Session(bind=self.bind)
+        else:
+            ValueError("FAILURE: Session is already open.")
             
-
     def close(self):
-        ''' closes the current database session if open. '''
+        """ closes the current database session if open. """
         if self.is_open():
             self._db.expunge_all()
             self._db.close()
             self._db = None
+        else:
+            ValueError("FAILURE: No Session to close.")
 
     def _clear(self):
-        ''' clears all tables '''
-        self.ensure_open()
-
+        """ clears all tables """
         for table in reversed(database.base.TableBase.metadata.sorted_tables):
             self._db.execute(table.delete())
-
         return self.commit()
 
     def commit(self):
-        ''' Commits changed content left in session.
-            Automatic rollback is performed if errror occurs.
-            Returns success of transaction. '''
+        """ Commits changed content left in session.
+            Automatic rollback is performed if errror occurs. """
         if self.is_open():
             try:
                 self._db.commit()
-                return True
             except exc.SQLAlchemyError as e:
-                print("FAILURE:", e)
                 self._db.rollback()
-                return False
+                raise e
         else:
-            print("Notification: Session not open, nothing to commit.")
-            return False
+            raise ValueError("FAILURE: Session has not been opened.")
 
     def is_open(self):
-        ''' Returns true if the current session is active, false otherwise. '''
-        return self._db != None
-
-    def ensure_open(self):
-        ''' checks if database session is open and opens if not. '''
-        if not self.is_open():
-            self.open()
+        """ Returns true if the current session is active, false otherwise. """
+        return self._db is not None
 
     def insert(self, obj):
-        ''' adds a database object to the current session. '''
-        if self.is_open():
-            self._db.add(obj)
-        else:
-            print("Notification: Session not open, nothing to add.")
-
+        """ adds a database object to the current session. """
+        self._db.add(obj)
+    
     def insert_all(self, obj_list):
-        ''' adds a list of database objects to the current session. '''
-        if self.is_open():
-            self._db.add_all(obj_list)
-        else:
-            print("Notification: Session not open, nothing to add.")
-
+        """ adds a list of database objects to the current session. """
+        self._db.add_all(obj_list)
+    
     def expunge(self, obj):
-        ''' removes a database object from the current session. '''
-        if self.is_open():
-            self._db.expunge(obj)
-        else:
-            print("Notification: Session not open, nothing to remove.")
-
+        """ removes a database object from the current session. """
+        self._db.expunge(obj)
+    
     def expunge_all(self, obj_list):
-        ''' removes a list of database objects from the current session. '''
-        if self.is_open():
-            for item in obj_list:
-                self._db.expunge(item)
-        else:
-            print("Notification: Session not open, nothing to remove.")
-
+        """ removes a list of database objects from the current session. """
+        for item in obj_list:
+            self._db.expunge(item)
+    
     def delete(self, obj):
-        ''' Helper function to encapsulate delete operations.
-            Performs automatic commit (see commit()).
-            Performs automatic expunge if commit unsuccessful (see expunge()).
-            Returns success of operation. '''
-        self.ensure_open()
+        """ Helper function to encapsulate delete operations.
+            Performs automatic commit (see commit()). """
         self._db.delete(obj)
-        if not self.commit():
-            self._db.expunge(obj)
-            return False
-        return True
+        self.commit()
     
     def query(self,*args):
-        ''' universal function for querying the database connection. '''
-        self.ensure_open()
+        """ universal function for querying the database connection. """
         return self._db.query(*args)
     
-    def select(self, table, filter):
-        ''' helper function for performing simple select operations. '''
-        return self.query(table).filter(filter)
-
-    def select_all(self, table):
-        ''' helper function for performing select operations with no filter. '''
+    def select(self, table, filter=None):
+        """ helper function for performing simple select operations. """
+        if filter is None:
+            return self.query(table)
         return self.query(table).filter(filter)
